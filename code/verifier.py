35a836b39a1dfc66a9107855bec1adab05e8420b
elnehr <104595482+elnehr@users.noreply.github.com>
2023-12-23 00:30:01 +0100

    fixed index

--- code/verifier.py
+++ code/verifier.py
@@ -173,13 +173,14 @@ def analyze(
             scheduler.step()
 
         index = 0
+        index2 = 0
         print("leaky_relu_index_list", leaky_relu_index_list)
         print("leaky_relu_slope_list", leaky_relu_slope_list)
         for parameter in polynet.parameters():
             if parameter.requires_grad:
                 # if leaky relu according to index, clamp with slope
                 if index in leaky_relu_index_list:
-                    if leaky_relu_slope_list[index] < 1:
+                    if leaky_relu_slope_list[index2] < 1:
                         print("case1")
                         parameter.data = parameter.data.clamp_(
                             leaky_relu_slope_list[index], 1
@@ -189,9 +190,10 @@ def analyze(
                         parameter.data = parameter.data.clamp_(
                             1, leaky_relu_slope_list[index]
                         )
-                    index += 1
+                    index2 += 1
                 else:
                     parameter.data = parameter.data.clamp_(0, 1)
+                index += 1
                 
 
     return result
